b4fe79aef53aa51aaf6d6c4088d3f4152d743c51
David Pham <davidpham@Davids-MacBook-Pro.local>
2016-07-24 23:23:11 -0400

    Removed dependency on Bootstrap and fixed requires

--- source/js/application.js
+++ source/js/application.js
@@ -1,17 +1,17 @@
-//= require "vendor/jquery"
-//= require "vendor/jquery.color"
-//= require "vendor/bootstrap-switch"
-//= require "vendor/bootstrap-scrollspy"
-//= require "vendor/bootstrap-dropdown"
-//= require "vendor/bootstrap-button"
-//= require "jquery-sortable"
+//= require "./vendor/jquery"
+//= require "./vendor/jquery.color"
+//= require "./vendor/bootstrap-switch"
+//= require "./vendor/bootstrap-scrollspy"
+//= require "./vendor/bootstrap-dropdown"
+//= require "./vendor/bootstrap-button"
+//= require "./jquery-sortable"
 //= require_directory "./examples/"
 
 $(function  () {
   if(!/test/.test(window.location.pathname))
-    $('body').scrollspy();
+    $('body').scrollspy()
   $('.show-code').on('click', function  () {
-    $(this).closest('.row').children('.example').slideToggle();
-  });
-  $('ol.default').sortable();
-});
+    $(this).closest('.row').children('.example').slideToggle()
+  })
+  $('ol.default').sortable()
+})
